adf1e04ed58743fd3a61fcabf0870935b25c04fb
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-17 17:47:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,6 +33,12 @@ sql = "SELECT * FROM dogs WHERE id = ?"
 result = DB[:conn].execute(sql, id)[0]
 Dog.new(result[0], result[1], result[2])
 end
+def self.find_by_name(name)
+sql = "SELECT * FROM dogs WHERE name = ?"
+result = DB[:conn].execute(sql, id)[0]
+Dog.new(result[0], result[1], result[2])
+end
+ 
  
   def update
     sql = <<-SQL
