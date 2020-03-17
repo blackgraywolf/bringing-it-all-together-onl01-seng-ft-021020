71f52eee868a864b008633ac9a121cb5a72fee15
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-17 19:22:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,8 +14,8 @@ class Dog
     }
     self.new(attributes_hash)
   end
-
-def saves
+#this is not used
+def save_example
     sql = <<-SQL
       INSERT INTO dogs (name, breed) VALUES (?, ?)
       SQL
@@ -31,10 +31,9 @@ def saves
 	self.update
 	 else
 	 sql = <<-SQL
-	 INSERT INTO dogs (name, breed)
-	 VALUES (?, ?)
+	 INSERT INTO dogs (name, breed) VALUES (?, ?)
  SQL
-	 DB[:conn].execute(sql, self.name, self.album)
+	 DB[:conn].execute(sql, self.name, self.breed)
 	 @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 	 end
 	 self
