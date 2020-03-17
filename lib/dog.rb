8c78dc2f9064d4b633fef67361cb800ef60c0f17
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-17 17:49:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 class Dog
-attr_accessor :name, :breed
-attr_reader :id
- 
-def initialize(id=nil, name, breed)
-@id = id
-@name = name
-@breed = breed
-end
- 
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    #id: nil, name:, breed:
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+  
 def save
 if self.id
 self.update
